c3cca95911fb30bbd12b8d2f2bb58798b7befaf0
Monica Gerard <mlgerardvla@gmail.com>
2020-03-25 04:21:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)# write code here
+    #side_array = [a, b, c]
+  end
+
+  def valid?
+    #if array.any? is true
+      #&&
+      #( a + b > c && b + c > a &&  c + a > b ) is true
+      #then kind()
+    #else
+      #raise error
+  end
+
+  def kind
+    #use array.uniq.length to determine how many unique values in array
+
+    # length 1  return :equilateral
+    # length 2 return :isosceles
+    # length 3 return :scalene
+  end
+
+
+  class TriangleError < StandardError
+
+  end
 end
